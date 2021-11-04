4c28d54fd56921f3260f97d0620cf6737483bc31
srinadhponugoti <srinadhpo141998@gmail.com>
2021-11-03 22:59:28 -0500

    final commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var animalRouter = require('./routes/animal');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
+
 
 var app = express();
 
@@ -21,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/animal', animalRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
